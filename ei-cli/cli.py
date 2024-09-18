039af44a3de89690f744f044c5fba0cec5e9a4a3
halpraka <halpraka@cisco.com>
2024-09-18 12:00:18 +0530

    Initial checkins

--- ei-cli/cli.py
+++ ei-cli/cli.py
@@ -15,50 +15,62 @@ cwd = os.getcwd()
 
 def main():
 
-  parser = argparse.ArgumentParser(description="EI Data Pipeline Orchestrator")
-  parser.add_argument('-v', '--verbosity', type=int, dest='verbosity', help='verbosity range [0-6]', default=2, required=False)
-
+  parser = argparse.ArgumentParser(description="EI Local Manager CLI Orchestrator (Version 1.0.0)")
+  # parser.add_argument('-v', '--verbosity', type=int, dest='verbosity', help='verbosity range [0-6]', default=2, required=False)
   command_parser = parser.add_subparsers(help="options", dest='command')
-  pipeline_parser = command_parser.add_parser('pipeline', help='Pipeline Operations (deploy, undeploy, status)')
+
+  # User related CLIs
+  user_parser = command_parser.add_parser('user', help='user operations (change-password)')
+  user_command_parser = user_parser.add_subparsers(dest='user_command')
+
+  user_change_pwd_parser = user_command_parser.add_parser('change-pwd', help='change user(s) password on the device(s)')
+  user_change_pwd_parser.add_argument('-f', '--file', type=str, dest='inventory_file', help='device(s) inventory file (yaml)')
+
+  # Agent related CLIs
+  agent_parser = command_parser.add_parser('agent', help='agent operations (status, reset)')
+  agent_command_parser = agent_parser.add_subparsers(dest='agent_command')
+
+  agent_reset_parser = agent_command_parser.add_parser('reset', help='reset the agent(s)')
+  agent_reset_parser.add_argument('-f', '--file', type=str, dest='inventory_file', help='device(s) inventory file (yaml)')
+
+  agent_status_parser = agent_command_parser.add_parser('status', help='get status of the agent(s)')
+  agent_status_parser.add_argument('-f', '--file', type=str, dest='inventory_file', help='device(s) inventory file (yaml)')
+
+  # Pipeline related CLIs
+  pipeline_parser = command_parser.add_parser('pipeline', help='pipeline operations (deploy, undeploy, status)')
   pipeline_command_parser = pipeline_parser.add_subparsers(dest='pipeline_command')
 
-  pipeline_deploy_parser = pipeline_command_parser.add_parser('deploy', help='Deploy single/multi data pipeline(s)')
-  pipeline_deploy_parser.add_argument('-i', '--ip', type=str, dest='device_ip', help='ip address of the device (mandatory for single pipeline config option)')
-  pipeline_deploy_parser.add_argument('-n', '--name', type=str, dest='pipeline_name', help='name of the data pipeline (mandatory for single pipeline config option)')
-  pipeline_deploy_parser.add_argument('-c', '--config-file', type=str, dest='single_pipeline_config', help='single data pipeline config file path (json)')
-  pipeline_deploy_parser.add_argument('-p', '--pipeline-file', type=str, dest='multi_pipeline_config', help='multi data pipeline config file path (yaml)')
+  pipeline_deploy_parser = pipeline_command_parser.add_parser('deploy', help='deploy data pipeline(s)')
+  pipeline_deploy_parser.add_argument('-f', '--file', type=str, dest='inventory_file', help='pipeline(s) inventory file (yaml)')
 
-  pipeline_undeploy_parser = pipeline_command_parser.add_parser('undeploy', help='UnDeploy single/multi data pipeline(s)')
-  pipeline_undeploy_parser.add_argument('-i', '--ip', type=str, dest='device_ip', help='ip address of the device (mandatory for single pipeline config option)')
-  pipeline_undeploy_parser.add_argument('-n', '--name', type=str, dest='pipeline_name', help='name of the data pipeline (mandatory for single pipeline config option)')
-  pipeline_undeploy_parser.add_argument('-c', '--config-file', type=str, dest='single_pipeline_config', help='single data pipeline config file path (json)')
-  pipeline_undeploy_parser.add_argument('-p', '--pipeline-file', type=str, dest='multi_pipeline_config', help='multi data pipeline config file path (yaml)')
+  pipeline_undeploy_parser = pipeline_command_parser.add_parser('undeploy', help='un-deploy data pipeline(s)')
+  pipeline_undeploy_parser.add_argument('-f', '--file', type=str, dest='inventory_file', help='pipeline(s) inventory file (yaml)')
 
-  pipeline_status_parser = pipeline_command_parser.add_parser('status', help='Get status for single/multi data pipeline(s)')
-  pipeline_status_parser.add_argument('-i', '--ip', type=str, dest='device_ip', help='ip address of the device (mandatory for single pipeline config option)')
-  pipeline_status_parser.add_argument('-n', '--name', type=str, dest='pipeline_name', help='name of the data pipeline (mandatory for single pipeline config option)')
-  pipeline_status_parser.add_argument('-c', '--config-file', type=str, dest='single_pipeline_config', help='single data pipeline config file path (json)')
-  pipeline_status_parser.add_argument('-p', '--pipeline-file', type=str, dest='multi_pipeline_config', help='multi data pipeline config file path (yaml)')
+  pipeline_status_parser = pipeline_command_parser.add_parser('status', help='get status of data pipeline(s)')
+  pipeline_status_parser.add_argument('-f', '--file', type=str, dest='inventory_file', help='pipeline(s) inventory file (yaml)')
 
   args = parser.parse_args()
-  logger.debug("Pipeline command running with args: %s", vars(args))
-
-  if args.verbosity == 0:
-    logging.basicConfig(level=logging.CRITICAL)
-  elif args.verbosity == 1:
-    logging.basicConfig(level=logging.WARNING)
-  elif args.verbosity == 2:
-    logging.basicConfig(level=logging.INFO)
-  elif args.verbosity >= 3:
-    logging.basicConfig(level=logging.DEBUG)
+  logger.debug("EI CLI command running with args: %s", vars(args))
 
   if args.command == 'pipeline':
     if args.pipeline_command == 'deploy':
-      on_deploy(vars(args), pipeline_deploy_parser)
+      on_pipeline_deploy(vars(args), pipeline_deploy_parser)
     elif args.pipeline_command == 'undeploy':
-      on_undeploy(vars(args), pipeline_undeploy_parser)
+      on_pipeline_undeploy(vars(args), pipeline_undeploy_parser)
     elif args.pipeline_command == 'status':
-      on_status(vars(args), pipeline_status_parser)
+      on_pipeline_status(vars(args), pipeline_status_parser)
+    else:
+      parser.print_help()
+  elif args.command == 'agent':
+    if args.agent_command == 'reset':
+      on_agent_reset(vars(args), agent_reset_parser)
+    elif args.agent_command == 'status':
+      on_agent_status(vars(args), agent_status_parser)
+    else:
+      parser.print_help()
+  elif args.command == 'user':
+    if args.user_command == 'change-pwd':
+      on_user_change_pwd(vars(args), user_change_pwd_parser)
     else:
       parser.print_help()
   else:
@@ -67,52 +79,30 @@ def main():
 def is_valid_string(string):
   return bool(string and string.strip())
 
-def validate_args(args):
-  is_single_dep = is_multiple_dep = False
-  if is_valid_string(args["single_pipeline_config"]) and is_valid_string(args["multi_pipeline_config"]):
-    print("\nPlease provide either single pipeline option or multiple pipeline option but not both\n")
-    logger.error("Invalid : both single pipeline option and multiple pipeline option are given")
-  elif not(is_valid_string(args["single_pipeline_config"])) and not(is_valid_string(args["multi_pipeline_config"])):
-    print("\nPlease provide at least single pipeline option or multiple pipeline option\n")
-    logger.error("Invalid : both single pipeline option and multiple pipeline option are not valid")
-  elif is_valid_string(args["single_pipeline_config"]):
-    if not is_valid_string(args["device_ip"]):
-      print("\nPlease provide valid ip address\n")
-      logger.error("Invalid Arg(s): device ip is null or empty")
-    elif not (is_valid_string(args["pipeline_name"])):
-      print("\nPlease provide valid pipeline name\n")
-      logger.error("Invalid Arg(s): pipeline name is null or empty")
-    else:
-      args_str = str(args["device_ip"]) + " " + str(args["pipeline_name"]) + " " + str(args["single_pipeline_config"])
-      logger.debug("Single pipeline deployment selected :" + args_str)
-      print("\nSingle pipeline deployment selected", args["device_ip"], args["pipeline_name"], args["single_pipeline_config"])
-      is_single_dep = True
-  elif is_valid_string(args["multi_pipeline_config"]):
-    logger.debug("Multi pipeline deployment selected :" + args["multi_pipeline_config"])
-    print("\nMulti pipeline deployment selected", args["multi_pipeline_config"])
-    is_multiple_dep = True
+def on_pipeline_deploy(args, parser):
+  logger.debug("Calling on_pipeline_deploy()")
+  inventory_file = args.get("inventory_file", "")
+  if is_valid_string(inventory_file):
+    logger.debug("Pipeline deployment selected", inventory_file)
   else:
-    logger.debug("Invalid pipeline deployment selected")
-    is_single_dep = is_multiple_dep = False
-  return is_single_dep, is_multiple_dep
-
-def on_deploy(args, parser):
-  logger.debug("Calling on_deploy()")
-  is_single_dep, is_multiple_dep = validate_args(args)
-  if is_single_dep:
-      print("\nSingle pipeline deployment selected", args["device_ip"], args["pipeline_name"], args["single_pipeline_config"])
-      # TODO
-  elif is_multiple_dep:
-    print("\nMulti pipeline deployment selected", args["multi_pipeline_config"])
-    # TODO
-  else:
+    print("\nPlease provide a valid pipeline inventory file option\n")
+    logger.error("Invalid : pipeline inventory file options :" + str(inventory_file))
     parser.print_help()
 
-def on_undeploy(args, parser):
-  print("on_undeploy ", args)
+def on_pipeline_undeploy(args, parser):
+  print("on_pipeline_undeploy", args)
 
-def on_status(args, parser):
-  print("on_status ", args)
+def on_pipeline_status(args, parser):
+  print("on_pipeline_status", args)
+
+def on_agent_status(args, parser):
+  print("on_agent_status", args)
+
+def on_agent_reset(args, parser):
+  print("on_agent_reset", args)
+
+def on_user_change_pwd(args, parser):
+  print("on_user_change_pwd", args)
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser(description='...')
